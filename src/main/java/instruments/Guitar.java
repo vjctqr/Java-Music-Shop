fd3660c56f383563015b45f2ffd780b907bf8b09
Victor <victorsetlhare@gmail.com>
2020-06-28 18:11:05 +0100

    guitar brand test

--- src/main/java/instruments/Guitar.java
+++ src/main/java/instruments/Guitar.java
@@ -1,4 +1,12 @@
 package instruments;
 
-public class Guitar {
+public class Guitar extends Instrument {
+
+    private String brand;
+    private String color;
+    private double price;
+
+    public Guitar(String brand, String color, double price){
+        super(brand, color, price);
+    }
 }
--- src/test/java/instruments/GuitarTest.java
+++ src/test/java/instruments/GuitarTest.java
@@ -1,4 +1,21 @@
 package instruments;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuitarTest {
+
+    private Guitar guitar;
+
+    @Before
+    public void before(){
+        guitar = new Guitar("Fender", "White", 575.00);
+    }
+
+    @Test
+    public void hasBrand(){
+        assertEquals("Fender", guitar.getBrand());
+    }
 }
