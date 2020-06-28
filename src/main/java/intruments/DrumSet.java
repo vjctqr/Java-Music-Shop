3a1310f3cfa04d3548b6c6af75370f163cfd2072
Victor <victorsetlhare@gmail.com>
2020-06-28 15:49:30 +0100

    initial commit

---
+++ src/main/java/intruments/DrumSet.java
@@ -0,0 +1,4 @@
+package intruments;
+
+public class DrumSet {
+}
---
+++ src/main/java/intruments/Instrument.java
@@ -0,0 +1,9 @@
+package intruments;
+
+public abstract class Instrument {
+
+    private String brand;
+    private String color;
+
+    public Instrument(String name, String color)
+}
