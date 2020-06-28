4296e9a5ea6986203f65fd73cd93696fdc958c11
Victor <victorsetlhare@gmail.com>
2020-06-28 18:30:41 +0100

    piano construct

--- src/main/java/instruments/Piano.java
+++ src/main/java/instruments/Piano.java
@@ -1,4 +1,12 @@
 package instruments;
 
-public class Piano {
+public class Piano extends Instrument{
+
+    private String brand;
+    private String color;
+    private double price;
+
+    public Piano(String brand, String color, double price){
+        super(brand, color, price);
+    }
 }
--- src/test/java/instruments/PianoTest.java
+++ src/test/java/instruments/PianoTest.java
@@ -1,4 +1,21 @@
 package instruments;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PianoTest {
+
+    private Piano piano;
+
+    @Before
+    public void before(){
+        piano = new Piano("Gewa", "White", 2399.00 );
+    }
+
+    @Test
+    public void hasBrand(){
+        assertEquals("Gewa", piano.getBrand());
+    }
 }
