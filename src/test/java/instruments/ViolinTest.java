791ab983fe982fc7601c3faa1b86f6a9e04d21ea
Victor <victorsetlhare@gmail.com>
2020-06-28 18:42:50 +0100

    violin brand test

--- src/test/java/instruments/ViolinTest.java
+++ src/test/java/instruments/ViolinTest.java
@@ -1,4 +1,21 @@
 package instruments;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ViolinTest {
+
+    private Violin violin;
+
+    @Before
+    public void before(){
+        violin = new Violin("Cremona", "Ebony", 189.99);
+    }
+
+    @Test
+    public void hasBrand(){
+        assertEquals("Cremona", violin.getBrand());
+    }
 }
